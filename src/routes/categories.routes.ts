347c671469652f1336e2b28f12581dec5ca0b019
Guilherme Varela <guilhermevareladossantos@gmail.com>
2021-06-23 02:41:27 -0300

    Aula 1.2.7 - Listando as categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -13,4 +13,9 @@ categoriesRoutes.post("/", (request, response) => {
   return response.status(201).send();
 });
 
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+  return response.json({ all });
+});
+
 export { categoriesRoutes };
