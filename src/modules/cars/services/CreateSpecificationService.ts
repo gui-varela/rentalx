381aceb5a83ff63d184dd4cbecee5abd5b91d61e
Guilherme Varela <guilhermevareladossantos@gmail.com>
2021-08-09 23:08:26 -0300

    Aula 1.4.3 - Criando os Use Case de Categoria

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,25 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute(): void {
-    console.log("TODO");
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const spececificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (spececificationAlreadyExists) {
+      throw new Error("Specification already Exists!");
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
   }
 }
 
--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,11 +1,11 @@
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CategoriesRepository } from "../../repositories/CategoriesRepository";
 
 interface IRequest {
   name: string;
   description: string;
 }
 
-class CreateCategoryService {
+class CreateCategoryUseCase {
   constructor(private categoriesRepository: CategoriesRepository) {}
 
   execute({ description, name }: IRequest): void {
@@ -19,4 +19,4 @@ class CreateCategoryService {
   }
 }
 
-export { CreateCategoryService };
+export { CreateCategoryUseCase };
