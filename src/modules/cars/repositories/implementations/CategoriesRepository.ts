683d4f32218338ac3370dc524936d8c6b05f1341
Guilherme Varela <guilhermevareladossantos@gmail.com>
2021-08-11 00:53:18 -0300

    Aula 1.4.7 - Separando os repositorios

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,8 +1,8 @@
-import { Category } from "../model/Category";
+import { Category } from "../../model/Category";
 import {
   ICategoriesRepository,
   ICreateRepositoryDTO,
-} from "./ICategoriesRepository";
+} from "../ICategoriesRepository";
 
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
