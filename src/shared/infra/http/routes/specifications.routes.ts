846b140a9b9d85c428f342aa29d8a2f7161cfdec
Guilherme Varela <guilhermevareladossantos@gmail.com>
2021-12-24 00:22:45 -0300

    Aula 1.3.7 - Criando middleware de administrador

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,18 +1,17 @@
 import { Router } from "express";
 
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
-
-import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
+import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
-specificationsRoutes.use(ensureAuthenticated);
-
 specificationsRoutes.post(
   "/",
   ensureAuthenticated,
+  ensureAdmin,
   createSpecificationController.handle
 );
 
