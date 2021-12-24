f4275f02edc3956896e28475ecdae3ea078aa05c
Guilherme Varela <guilhermevareladossantos@gmail.com>
2021-12-23 23:56:22 -0300

    Aula 1.3.6 - Criando seed de usuário

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,15 +3,15 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "@shared/infra/typeorm";
-
 import { AppError } from "@shared/errors/AppError";
+import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
 import "@shared/container";
 
+createConnection();
 const app = express();
 
 app.use(express.json());
