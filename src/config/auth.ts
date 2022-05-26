5e466d320f46e4c28f485ecc2866a7bca68e9f99
Guilherme Varela <guilhermevareladossantos@gmail.com>
2022-05-25 22:30:54 -0300

    Aula 1.2.2 - Refatorando Autenticação do usuário

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "c7da0c903543e4c21e14a9a2993b1274",
+  expires_in_token: "15m",
+  secret_refresh_token: "0e8d537030da0c658133b628bd25d71a",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
