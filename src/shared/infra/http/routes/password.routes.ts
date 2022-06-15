c6323ac0c1541ba4b8e239136a06981f40e18987
Guilherme Varela <guilhermevareladossantos@gmail.com>
2022-06-15 18:22:48 -0300

    Aula 1.3.4 - Caso de uso de reset de senha

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 
+import { ResetPasswordController } from "@modules/accounts/useCases/resetPassword/ResetPasswordController";
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 
 const passwordRoutes = Router();
 
 const sendForgottenPasswordMailController =
   new SendForgotPasswordMailController();
+const resetPasswordController = new ResetPasswordController();
 
 passwordRoutes.post("/forgot", sendForgottenPasswordMailController.handle);
+passwordRoutes.post("/reset", resetPasswordController.handle);
 
 export { passwordRoutes };
